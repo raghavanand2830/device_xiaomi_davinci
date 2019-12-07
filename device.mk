5fad342c075ec7ad5f28bca2193f4f483c70b301
Alex Yustasov <alex9yust@gmail.com>
2019-12-07 21:06:18 +0300

    Fixes to NFC configuration

--- device.mk
+++ device.mk
@@ -136,6 +136,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     com.gsma.services.nfc \
+    com.nxp.nfc.nq \
+    libnqnfc-nci \
+    nqnfcee_access.xml \
+    nqnfcse_access.xml \
     NfcNci \
     SecureElement \
     Tag
