789b8207e6774a3148700d2f3125f6648341b8ea
Demon000 <demonsingur@gmail.com>
2019-12-31 01:06:45 +0100

    FOD: improve semantics
     * do not use (void) to silence unused parameters
     * do not specify namespace for IFingerprintInscreenCallback

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -158,7 +158,7 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
     return Void();
 }
 
-Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t /* brightness */) {
     float alpha;
     int realBrightness = get(BRIGHTNESS_PATH, 0);
 
@@ -168,7 +168,6 @@ Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
         alpha = 1.0 - pow(realBrightness / 1680.0, 0.455);
     }
 
-    (void) brightness;
     return 255 * alpha;
 }
 
@@ -176,8 +175,7 @@ Return<bool> FingerprintInscreen::shouldBoostBrightness() {
     return false;
 }
 
-Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>& callback) {
-    (void) callback;
+Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>& /* callback */) {
     return Void();
 }
 
