098787cfb6663d2389ee6dbd4081073060884181
RealSchnedi <schnedi3@gmail.com>
2020-01-31 20:14:11 +0100

    Bluetooth: add default name

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -21,15 +21,15 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
+#define BTM_DEF_LOCAL_NAME "Mi 9T"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
 #define BTM_WBS_INCLUDED   TRUE
 #define BTIF_HF_WBS_PREFERRED   TRUE
 #define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
+// Skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
-
-/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
+// Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
 #define AVDT_NUM_SEPS 12
 #endif
