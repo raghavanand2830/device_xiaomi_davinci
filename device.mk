428d6c1c342e11db3733cf6ea76fcb8ef523080a
Dyneteve <dyneteve@pixelexperience.org>
2020-02-07 18:02:40 +0200

    WiFi: update WCNSS_qcom_cfg and import wifi_concurrency_cfg from coral

--- device.mk
+++ device.mk
@@ -194,7 +194,8 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/wifi_concurrency_cfg.txt:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/wifi/wifi_concurrency_cfg.txt
 
 # WiFi Display
 PRODUCT_PACKAGES += \
