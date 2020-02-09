8dc319590bb060e263629448e4d6a123931d8d6c
RealSchnedi <schnedi3@gmail.com>
2020-02-09 15:18:10 +0100

    We are Syberia now

--- syberia_davinci.mk
+++ syberia_davinci.mk
@@ -8,13 +8,13 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 
 # Inherit some common stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/syberia/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := davinci
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_davinci
+PRODUCT_NAME := syberia_davinci
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
